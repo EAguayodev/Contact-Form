e28dfbfb7c9b95cf7e6f0a2fcadea1699880dccd
Eric Aguayo <computergeek90@protonmail.com>
2025-02-24 14:20:22 -0600

    mobile responsiveness for iphones test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const form = document.getElementById('form');
 const btnSubmit = document.getElementById('btn-submit');
 
-btnSubmit.addEventListener('submit', function(){
+btnSubmit.addEventListener('submit', function(e){
+    const email = document.getElementById('email');
+    e.preventDefault();
+  if (email === '0' || !email.checkValidity()) {
     
+  } else if(){
+    
+  } else{
+
+  }
 })
+
+
+function btnValidator(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
